6677010fa532297b5739ea77bc3b9ba2ce0634cb
hannahjuett <hannah.juett@gmail.com>
2019-08-15 16:22:35 -0400

    fixed id annotations

--- src/main/java/co/grandcircus/Soundtrack_of_my_life/entity/User.java
+++ src/main/java/co/grandcircus/Soundtrack_of_my_life/entity/User.java
@@ -1,7 +1,16 @@
 package co.grandcircus.Soundtrack_of_my_life.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class User {
 	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String username;
 	private String password;
